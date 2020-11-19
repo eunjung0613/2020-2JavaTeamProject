4b6528a2cfd2d28196066a515d328c04641c0a94
eunjung0613 <dmswjd7281@naver.com>
2020-11-20 03:38:38 +0900

    공지사항의 대부분 기능 제작 완료.

--- reservation/src/Notice/Modify.java
+++ reservation/src/Notice/Modify.java
@@ -2,6 +2,11 @@ package Notice;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,11 +15,37 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class Modify extends JFrame{
-	public Modify() {
+	public Modify(int num) {
+		Connection conn = null;
+		String sql = null;
+		ResultSet rs = null;
+		Statement stmt = null;
+		DTO dto = new DTO();
+		dto.setNum(num);
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); //드라이버 로딩		
+		}catch(ClassNotFoundException E) {
+	         System.err.print("ClassNotFindException: ");
+	      }
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://prmodoris.c7sj6azpcoop.ap-northeast-2.rds.amazonaws.com :3306/prmodoris", "admin", "prmodoris");
+			System.out.println("DB 연결 완료");
+			sql = "SELECT * FROM prmodoris.Notice order by num="+dto.getNum();
+			stmt=(Statement)conn.createStatement();
+			rs=stmt.executeQuery(sql);
+			while(rs.next()) {
+				dto.setMemo(rs.getString("memo"));
+				dto.setTitle(rs.getString("title"));
+				dto.setId(rs.getString("id"));
+			}
+		}catch(SQLException e1) {
+			System.out.println("에러: "+e1);
+		}
+		
 		JFrame main = new JFrame("수정");
 		JButton ok = new JButton("확인");
-		JTextField title = new JTextField();
-		JTextArea cont = new JTextArea();
+		JTextField title = new JTextField(dto.getTitle());
+		JTextArea cont = new JTextArea(dto.getMemo());
 		
 		main.setLayout(null);
 		main.setBounds(10, 10, 800, 800);
@@ -37,7 +68,37 @@ public class Modify extends JFrame{
 				if(result == JOptionPane.CLOSED_OPTION) {
 					
 				}else if(result==JOptionPane.YES_OPTION) {
+					Connection conn = null;
+					String sql = null;
+					ResultSet rs = null;
+					Statement stmt = null;
+					String newtitle = title.getText();
+					String newMemo = cont.getText();
+					dto.setTitle(newtitle);
+					dto.setMemo(newMemo);
+					try {
+						Class.forName("com.mysql.cj.jdbc.Driver"); //드라이버 로딩		
+					}catch(ClassNotFoundException E) {
+				         System.err.print("ClassNotFindException: ");
+				      }
+					try {
+						conn = DriverManager.getConnection("jdbc:mysql://prmodoris.c7sj6azpcoop.ap-northeast-2.rds.amazonaws.com :3306/prmodoris", "admin", "prmodoris");
+						System.out.println("DB 연결 완료");
+						sql = "SELECT num FROM prmodoris.Notice order by num desc limit 1";
+						stmt=(Statement)conn.createStatement();
+						rs=stmt.executeQuery(sql);
+							sql = "update prmodoris.Notice set title='"+dto.getTitle()+"',memo='"+dto.getMemo()+"' where num="+dto.getNum();
+							stmt=(Statement)conn.createStatement();
+							int affect = stmt.executeUpdate(sql);
+							if(affect==1) {
+								JOptionPane.showMessageDialog(null, "수정이 완료되었습니다.","수정 완료",JOptionPane.INFORMATION_MESSAGE);
+							}
+						}catch(SQLException e1) {
+						System.out.println("에러: "+e1);
+					}
 					main.dispose();
+					new NoticeGUI();
+					
 					//수정해서 DB작업
 				}else {
 					//사용자가 아니오 선택시 작업 없음.
--- reservation/src/Notice/add.java
+++ reservation/src/Notice/add.java
@@ -8,9 +8,12 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import java.sql.*;
+import java.sql.*;
 
 public class add extends JFrame{
 	public add() {
+		
 		JFrame main = new JFrame("글쓰기");
 		JButton ok = new JButton("확인");
 		JTextField title = new JTextField("제목");
@@ -33,20 +36,76 @@ public class add extends JFrame{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				int result = JOptionPane.showConfirmDialog(null,"등록하시겠습니까?","등록확인",JOptionPane.YES_NO_OPTION);
-				if(result == JOptionPane.CLOSED_OPTION) {
-					//닫힘 버튼 클릭시 아무일도 발생하지 않음.
+				Connection conn = null;
+				String sql = null;
+				ResultSet rs = null;
+				Statement stmt = null;
+				String text = cont.getText();
+				String title2 = title.getText();
+				
+				if(text.equals("내용을 입력하세요") || title2.equals("제목")) {
+					JOptionPane.showMessageDialog(null, "제목이나 내용이 입력되지 않았습니다.","오류!",JOptionPane.ERROR_MESSAGE);
+				}else {
+					int result = JOptionPane.showConfirmDialog(null,"등록하시겠습니까?","등록확인",JOptionPane.YES_NO_OPTION);
+					if(result == JOptionPane.CLOSED_OPTION) {
+						//닫힘 버튼 클릭시 아무일도 발생하지 않음.
+					}
+					else if(result == JOptionPane.YES_OPTION) {
+						//yes버튼 클릭시 등록 작업 진행 DB 등록 하는 작업
+						try {
+							Class.forName("com.mysql.cj.jdbc.Driver"); //드라이버 로딩		
+						}catch(ClassNotFoundException E) {
+					         System.err.print("ClassNotFindException: ");
+					      }
+						try {
+							conn = DriverManager.getConnection("jdbc:mysql://prmodoris.c7sj6azpcoop.ap-northeast-2.rds.amazonaws.com :3306/prmodoris", "admin", "prmodoris");
+							System.out.println("DB 연결 완료");
+							int lastnum2 = Search();
+							sql = "SELECT num FROM prmodoris.Notice order by num desc limit 1";
+							stmt=(Statement)conn.createStatement();
+							rs=stmt.executeQuery(sql);
+								sql = "insert into prmodoris.Notice VALUES ("+lastnum2+",'admin','"+title2+"','"+text+"')";
+								stmt=(Statement)conn.createStatement();
+								int affect = stmt.executeUpdate(sql);
+								if(affect==1) {
+									JOptionPane.showMessageDialog(null, "등록이 완료되었습니다.","등록 완료",JOptionPane.INFORMATION_MESSAGE);
+								}
+							}catch(SQLException e1) {
+							System.out.println("에러: "+e1);
+						}
+						new NoticeGUI();
+						main.dispose();
+					}
+					else {
+						//NO버튼 클릭시 쓰던 것 계속 작성.
+					}
 				}
-				else if(result == JOptionPane.YES_OPTION) {
-					//yes버튼 클릭시 등록 작업 진행 DB 등록 하는 작업
-					main.dispose();
+			}});
+		}
+	public int Search() {
+		Connection conn = null;
+		String sql = null;
+		ResultSet rs = null;
+		Statement stmt = null;
+		int lastnum = 0;
+		int lastnum2 = 0;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); //드라이버 로딩		
+		}catch(ClassNotFoundException E) {
+	         System.err.print("ClassNotFindException: ");
+	      }
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://prmodoris.c7sj6azpcoop.ap-northeast-2.rds.amazonaws.com :3306/prmodoris", "admin", "prmodoris");
+			System.out.println("DB 연결 완료");
+			sql = "SELECT num FROM prmodoris.Notice order by num desc limit 1";
+			stmt=(Statement)conn.createStatement();
+			rs=stmt.executeQuery(sql);
+			while(rs.next()) {
+				lastnum = rs.getInt("num");
 				}
-				else {
-					//NO버튼 클릭시 쓰던 것 계속 작성.
-				}
-			}
-			
-		});
+			lastnum2 = lastnum+1;
+		}catch(SQLException e1) {
+			System.out.println("에러: "+e1);
+		}return lastnum2;
 	}
-	
 }
